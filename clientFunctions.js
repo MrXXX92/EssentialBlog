21d5eb57b344e8eab2be23be88e68041737c5e1d
Michael Kerkhoff <MichaelKerkhoff@web.de>
2014-12-10 23:26:12 +0100

    XAMP installiert, DB eingerichtet und Einarbeitung in Javascript

---
+++ clientFunctions.js
@@ -0,0 +1,22 @@
+﻿function toogleComments(classId)
+{
+
+}
+
+function hideComments(classId)
+{
+	var x = document.getElementsByClassName("classId");
+	var i;
+		for (i = 0; i < x.length; i++) {
+			x[i].hide()
+		}
+}
+
+function showComments(classId)
+{
+	var x = document.getElementsByClassName("classId");
+	var i;
+		for (i = 0; i < x.length; i++) {
+			x[i].hide()
+		}
+}
