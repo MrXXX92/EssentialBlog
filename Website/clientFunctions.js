4312ab76995f1d0eb48219b398ef9f4b5d7ec80a
Michael Kerkhoff <MichaelKerkhoff@web.de>
2014-12-18 21:44:37 +0100

    Kommentare können nun ausgeklappt und wieder eingeklappt werden - format.css: Kommentare sind initial eingeklappt - clientFunctions.js: Scricptfehler beseitigt; Es werden nun andere Eigenschaften im DOM geändert - Aufwände aktualisiert

--- Website/clientFunctions.js
+++ Website/clientFunctions.js
@@ -1,35 +1,33 @@
 function hideComments(articleId)
 {
-	var prefix = "comment";
-	var comments = document.getElementsByClassName(prefix.concat(articleId));
+	var elementId = "comment" + articleId;
+	var comments = document.getElementsByClassName(elementId);
 	var i;
 	for (i = 0; i < comments.length; i++) {
-		comments[i].hide()
+		comments[i].style.visibility = "hidden";
+		comments[i].style.display = "none";
 	}
 	
-	var prefix = "HideComment";
-	document.getElementById(prefix.concat(articleId)).hide()
-	var prefix = "ShowComment";
-	document.getElementById(prefix.concat(articleId)).show()
+	document.getElementById("ShowComment" + articleId).style.visibility = "visible";
+	document.getElementById("ShowComment" + articleId).style.display = "block";
+	document.getElementById("HideComment" + articleId).style.visibility = "hidden";
+	document.getElementById("HideComment" + articleId).style.display = "none";
 }
 
 function showComments(articleId)
 {
-	alert("showCommentsEntered");
-	alert("articleId: " + articleId);
-	
 	var elementId = "comment" + articleId;
-	alert("elementId: " + elementId);
-	
 	var comments = document.getElementsByClassName(elementId);
 	var i;
-	for (i = 0; i < comments.length; i++) {
-		alert("Show element nr " + (i+1));
-		comments[i].show()
+	if (comments.length > 0) {
+		for (i = 0; i < comments.length; i++) {
+			comments[i].style.visibility = "visible";
+			comments[i].style.display = "block";
+		}
+		
+		document.getElementById("ShowComment" + articleId).style.visibility = "hidden";
+		document.getElementById("ShowComment" + articleId).style.display = "none";
+		document.getElementById("HideComment" + articleId).style.visibility = "visible";
+		document.getElementById("HideComment" + articleId).style.display = "block";
 	}
-	
-	document.getElementById("ShowComment" + articleId).hide()
-	document.getElementById("HideComment" + articleId).show()
-	
-	alert("showCommentsLeft");
 }
